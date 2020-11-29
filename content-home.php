7916a2221a383cdd05b04ae9848b0a0489846fda
Chloepottier <chloe.pottier@free.fr>
2020-11-29 20:42:02 +0100

    structure services

--- content-home.php
+++ content-home.php
@@ -1,20 +1,33 @@
 <section id="presentation" class="container-fluid">
+    <div class="container ">
+        <div class="row py-5 d-flex bg-guitare">
+            <div class="col-12 text-center pt-5 pb-3">
+                <!--title et content administrable-->
+                <h2 class="text-prune-dark pb-5 font-size-36">Akaleya, la solution adaptée à vos besoins print et web</h2>
+                <p class="text-prune font-size-18">Quelque soit votre projet, vos envies ou vos besoins,
+                    nous pouvons construire une solution à votre image. </p>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="services" class="container-fluid bg-prune-dark text-white">
+    <div class="container h-100">
+        <!-- bg en diagonal voir tete de com-->
+        <div class="row py-5 d-flex ">
+            <div class="col-12 text-center pt-5 pb-3">
+                <!--title et content administrable-->
+                <h2 class=" pb-5 font-size-36">Les services proposés</h2>
+            </div>
+            <?php include "template-parts/content/services.php"; ?>
+        </div>
+</section>
+<section id="portfolio" class="container-fluid">
     <div class="container h-100">
 
     </div>
 </section>
-<section id="services" class="container-fluid bg-prune">
-    <div class="container h-100">
-        
-    </div>
-</section>
-<section id="portfolio" class="container-fluid">
-    <div class="container h-100">
-        
-    </div>
-</section>
 <section id="contact" class="container-fluid bg-prune">
     <div class="container h-100">
-        
+
     </div>
 </section>
---
+++ template-parts/content/services.php
@@ -0,0 +1,23 @@
+<!-- loop-->
+<div class="col-12 col-md-3">
+    <img src="" alt="" title="" width="" height="" />
+    <h3>création graphiques</h3>
+    <p>Réalisation de logos, carte de visite, flyers, catalogues, brochures, photo-montage, plan technique, pré-presse...
+    </p>
+</div>
+<div class="col-12 col-md-3">
+    <img src="" alt="" title="" width="" height="" />
+    <h3>Site internet</h3>
+    <p></p>
+</div>
+<div class="col-12 col-md-3">
+    <img src="" alt="" title="" width="" height="" />
+    <h3>Boutique en ligne</h3>
+    <p></p>
+</div>
+<div class="col-12 col-md-3">
+    <img src="" alt="" title="" width="" height="" />
+    <h3>Conseils et accompagnement</h3>
+    <p>Akaleya est là pour votre orienter tout au long de votre projet . </p>
+</div>
+</div>
