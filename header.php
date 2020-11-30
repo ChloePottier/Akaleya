4959cc1270d26631ce24cbb32d8469f5510ea0e7
Chloepottier <chloe.pottier@free.fr>
2020-11-30 14:31:09 +0100

    modif nav bar function js

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
         <?php include 'template-parts/navigation/navigation-burger.php'; ?>
         <?php include 'template-parts/header/logo-header.php'; ?>
     </div>
-    <div class="container-fluid  nav-top d-none d-md-block nav-top bg-white position-absolute" id="navbar">
+    <div class="container-fluid  nav-top bg-white " id="navbar">
         <?php include 'template-parts/navigation/navigation-top.php'; ?>
     </div>
 </header>
