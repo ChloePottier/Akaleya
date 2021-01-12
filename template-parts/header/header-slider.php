627e912b42895ed8a8c8cf9581557342070a77cc
Chloepottier <chloe.pottier@free.fr>
2021-01-12 15:52:30 +0100

    loop slider

--- template-parts/header/header-slider.php
+++ template-parts/header/header-slider.php
@@ -1,22 +1,20 @@
 <div class="container mt-7" id="slider-header">
-            <div class="row">
-                <!-- Conteneur du slider -->
-                <div class="col slider-wrapper my-auto ">
-                    <nav class="slider-nav position-absolute ">
-                        <span  class="slider-prev mt-5"></span>
-                        <span class="slider-next mt-5"></span>
-                        <span class="clear"></span>
-                    </nav>
-                    <div class="slider-items text-white text-center font-size-24">
-                        <h1 class=" text-shadow"> Créations graphiques</h1>
-                        <p class="w-75 mx-auto text-shadow">Logos, cartes de visite, flyers, catalogues, retouches d’images, ...</p>
-                    </div>
-                    <div class="slider-items  text-white text-center font-size-24">
-                        <h1 class=" text-shadow">Sites internet</h1>
-                        <p class="w-75 mx-auto text-shadow">Sites vitrine, sites de e-commerce, développement web, ...</p>
-                    </div>
-                  
+    <div class="row">
+        <!-- Conteneur du slider -->
+        <div class="col slider-wrapper my-auto ">
+            <nav class="slider-nav position-absolute ">
+                <span class="slider-prev mt-5"></span>
+                <span class="slider-next mt-5"></span>
+                <span class="clear"></span>
+            </nav>
+            <?php $loop = new WP_Query(array('post_type' => 'slider'));
+            while ($loop->have_posts()) : $loop->the_post();?>
+                <div class="slider-items text-white text-center font-size-24">
+                    <h1 class=" text-shadow"><?php the_title(); ?></h1>
+                    <p class="w-75 mx-auto text-shadow"><?php the_content(); ?></p>
                 </div>
-
-            </div>
+            <?php endwhile; ?>
         </div>
+
+    </div>
+</div>
