df318314cc1330bab25a09f3cacae12e23aea2d9
Chloepottier <chloe.pottier@free.fr>
2021-01-13 11:54:13 +0100

    condition affichage lien realisation

--- template-parts/content/content-portfolio.php
+++ template-parts/content/content-portfolio.php
@@ -2,9 +2,16 @@
 if ($loop->have_posts()) :
     while ($loop->have_posts()) : $loop->the_post(); ?>
         <div class="col-12 col-sm-6 col-lg-4 d-flex flex-column  mb-3">
-            <a href="<?php the_field('url_realisation'); ?>" target="_blank">
+            <?php
+            $url = get_field('url_realisation');
+            if (empty($url)) : ?>
                 <img src="<?php the_field('image_realisation'); ?>" width="370" height="250" alt="" title="" class="responsive" />
-            </a>
+            <?php else : ?>
+                <a href="<?php the_field('url_realisation'); ?>" target="_blank">
+                    <img src="<?php the_field('image_realisation'); ?>" width="370" height="250" alt="" title="" class="responsive" />
+                </a>
+            <?php endif; ?>
+
             <h3 class="pt-3 text-prune-dark"><?php the_title(); ?></h3>
             <div class="text-prune"><?php the_content(); ?></div>
         </div>
