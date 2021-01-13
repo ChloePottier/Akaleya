4108df2b9c862756cf91a74ecf94550e4dd8d96e
Chloepottier <chloe.pottier@free.fr>
2021-01-13 11:00:52 +0100

    portfolio loop

--- template-parts/content/content-portfolio.php
+++ template-parts/content/content-portfolio.php
@@ -2,11 +2,11 @@
 if ($loop->have_posts()) :
     while ($loop->have_posts()) : $loop->the_post(); ?>
         <div class="col-12 col-sm-6 col-lg-4 d-flex flex-column  mb-3">
-            <a href="">
-                <img src="img/rumbleinn-min.jpg" width="370" height="250" alt="" title="" class="responsive" />
+            <a href="<?php the_field('url_realisation'); ?>">
+                <img src="<?php the_field('image_realisation'); ?>" width="370" height="250" alt="" title="" class="responsive" />
             </a>
-            <h3 class="pt-3 text-prune-dark">Rumble Inn Studio</h3>
-            <p class="text-prune">Création de la charte graphique, création du thème et personnalisation du back-office pour que chaques pages soit administrables</p>
+            <h3 class="pt-3 text-prune-dark"><?php the_title(); ?></h3>
+            <div class="text-prune"><?php the_content(); ?></div>
         </div>
 <?php endwhile;
 else :
