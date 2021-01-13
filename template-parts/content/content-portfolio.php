146f124c4b57a7a9a4afeb12580ce1a2392cbdfa
Chloepottier <chloe.pottier@free.fr>
2021-01-13 11:25:07 +0100

    realisations

--- template-parts/content/content-portfolio.php
+++ template-parts/content/content-portfolio.php
@@ -2,7 +2,7 @@
 if ($loop->have_posts()) :
     while ($loop->have_posts()) : $loop->the_post(); ?>
         <div class="col-12 col-sm-6 col-lg-4 d-flex flex-column  mb-3">
-            <a href="<?php the_field('url_realisation'); ?>">
+            <a href="<?php the_field('url_realisation'); ?>" target="_blank">
                 <img src="<?php the_field('image_realisation'); ?>" width="370" height="250" alt="" title="" class="responsive" />
             </a>
             <h3 class="pt-3 text-prune-dark"><?php the_title(); ?></h3>
