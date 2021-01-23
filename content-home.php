418cae893350733418516726957f0925d7ac012a
Chloepottier <chloe.pottier@free.fr>
2021-01-23 13:44:02 +0100

    test background diagonale

--- content-home.php
+++ content-home.php
@@ -14,6 +14,7 @@
                 </div>
             </div>
         </section>
+        <div class="container-fluid diagonale"></div>
     </scroll-page>
     <scroll-page id="page-2">
             <!-- <svg version="1.0" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="138.102px" viewBox="0 0 1024 138.102" enable-background="new 0 0 1024 138.102" xml:space="preserve">
