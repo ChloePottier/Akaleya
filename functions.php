606076b1a1d75381c12d4bed2ebae7f9671ce799
Chloepottier <chloe.pottier@free.fr>
2021-07-29 18:37:38 +0200

    mot de passe perdu

--- functions.php
+++ functions.php
@@ -193,5 +193,11 @@ function my_sharing_buttons($content) {
         }
         return $content;
 };
-// Formulaire de connexion
+// Mot de passe perdu
+// Ajouter le lien pour récupérer le mot de passe, si l'utilisateur ne s'en souvient plus
+add_filter( 'login_form_bottom', 'lien_mot_de_passe_perdu' );
+function lien_mot_de_passe_perdu( $formbottom ) {
+	$formbottom .= '<a href="' . wp_lostpassword_url() . '">Mot de passe perdu ?</a>';
+	return $formbottom;
+}
 
