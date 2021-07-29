c2a0e30b81c29d878eee3c505a95f5e5e0a0b121
Chloepottier <chloe.pottier@free.fr>
2021-07-29 16:59:38 +0200

    CPT hooks & functions

--- hooks.php
+++ hooks.php
@@ -3,10 +3,16 @@ add_action('wp_enqueue_scripts', 'akaleya_styles_scripts');
 add_filter('upload_mimes', 'wpm_myme_types', 1, 1);
 add_theme_support( 'post-thumbnails' );
 add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
-add_action( 'init', 'init_cpt_akaleya' );
+add_action( 'init', 'cpt_slider_init' );
+add_action( 'init', 'cpt_presentation_init' );
+add_action( 'init', 'cpt_services_init' );
+add_action( 'init', 'cpt_portfolio_init' );
+add_action( 'init', 'category_realisation' );
+add_action( 'init', 'cpt_link_init' );
 // ajout format d'image      
 if(function_exists('add_theme_support')):
     add_image_size('thumbnail_portfolio_sm',545,368,false);
     add_image_size('thumbnail_portfolio',370,250,false);    
+
 endif;
 add_filter( 'the_content', 'my_sharing_buttons');
