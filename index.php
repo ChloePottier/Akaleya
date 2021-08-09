d41d55154add2e11237ea89050801f1b28cce52b
ChloePottier <chloe.pottier@free.fr>
2021-08-09 16:24:03 +0200

    rôles wp wc ps et articles

--- index.php
+++ index.php
@@ -15,15 +15,31 @@ get_template_part('template-parts/header/header', 'page');?>
             $userRole = $user->roles[0];
             $categorieTabArticle = get_the_category();
             $categorieNameArticle = $categorieTabArticle[0];
-            print_r($categorieNameArticle);
+            // print_r($categorieNameArticle);
+            echo 'Role Utilisateur :'.$userRole.'<br/>';
+            echo'Category article :';
            the_category();
-            if (is_user_logged_in() && $userRole == 'abonne_wordpress'  OR current_user_can('edit_posts') ):
-                    if($categorieNameArticle->name == 'Utilisateurs Wordpress'):
-                        get_template_part('template-parts/content/content','loop');  
-                    else:
-                        get_template_part('template-parts/content/content','private');
-                    endif; 
-            elseif((is_user_logged_in() && $userRole == 'abonne_woocommerce'  OR current_user_can('edit_posts') )):
+            if (is_user_logged_in() && $userRole == 'abonne_wordpress' OR current_user_can('edit_posts') ):
+                if($categorieNameArticle->name == 'Utilisateurs Wordpress'):
+                    get_template_part('template-parts/content/content','loop');  
+                else:
+                    get_template_part('template-parts/content/content','private');
+                endif; 
+            elseif(is_user_logged_in() && $userRole == 'abonne_woocommerce'  OR current_user_can('edit_posts') ):
+
+                if($categorieNameArticle->name == 'Utilisateurs Woocommerce'):
+                    get_template_part('template-parts/content/content','loop'); 
+                elseif($categorieNameArticle->name == 'Utilisateurs Wordpress'):
+                        get_template_part('template-parts/content/content','loop');
+                else:
+                    get_template_part('template-parts/content/content','private');
+                endif; 
+            elseif(is_user_logged_in() && $userRole == 'abonne_prestashop'  OR current_user_can('edit_posts') ):
+                if($categorieNameArticle->name == 'Utilisateurs Prestashop'):
+                    get_template_part('template-parts/content/content','loop'); 
+                else:
+                    get_template_part('template-parts/content/content','private');
+                endif;
             else:
                 echo "<h1 class='text-prune-dark pt-0 pt-sm-5'>Article réservé aux abonnés</h1>";
                 echo '<a href="'.home_url('/').'espace-membres">Connectez-vous pour lire ce contenu.</a>';
