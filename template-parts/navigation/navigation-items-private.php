5e0deb049e09d4b196cc89fbb296a2882bf97eeb
ChloePottier <chloe.pottier@free.fr>
2021-08-11 10:31:32 +0200

    menu top connecté ou non

---
+++ template-parts/navigation/navigation-items-private.php
@@ -0,0 +1,25 @@
+    <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+        <a href='<?php echo esc_url( home_url( "/" ) ); ?>/espace-membres' class='text-prune-dark' itemprop='item'>
+            <span itemprop='name'>Espace privé</span><meta itemprop='position' content='1'/>
+        </a>
+    </li>
+    <!-- A activer quand formulaire modification sera créé -->
+    <!-- <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+        <a href='
+        <?php //echo esc_url( home_url( "/" ) ); ?>
+        ' class='text-prune-dark' itemprop='item'>
+            <span itemprop='name'>Mon compte</span><meta itemprop='position' content='2'/>
+        </a>
+    </li> -->
+    <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+        <a href='<?php echo esc_url( home_url( "/" ) ); ?>#section-contact' class='text-prune-dark' itemprop='item'>
+            <span itemprop='name'>Contact</span><meta itemprop='position' content='3'/>
+        </a>
+    </li>
+    <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem'>
+        <a href='<?php echo wp_logout_url(site_url('/')) ?>' class='text-prune-dark' itemprop='item'>
+            <span itemprop='name'>Se déconnecter</span>
+            <meta itemprop='position' content='4'/>
+        </a>
+    </li>
+    
--- template-parts/navigation/navigation-top.php
+++ template-parts/navigation/navigation-top.php
@@ -6,7 +6,13 @@
         <div class='col d-flex justify-content-end'>
             <nav class='menu-nav text-right d-none d-md-flex' >
                 <ul class='  d-flex justify-content-end font-family-bebas font-size-21 align-items-center mb-0 my-3' itemscope itemtype='https://schema.org/BreadcrumbList'>
-                    <?php  get_template_part('template-parts/navigation/navigation', 'items'); ?>                    
+                    <?php
+                     if(is_user_logged_in()):
+                        get_template_part('template-parts/navigation/navigation', 'items-private');
+                    else :
+                        get_template_part('template-parts/navigation/navigation', 'items');
+                    endif;
+                         ?>                    
                 </ul>
             </nav>
         </div>
