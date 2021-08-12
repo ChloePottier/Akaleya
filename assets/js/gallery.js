6d065fc61311e01509b439656685efed824c4ba6
ChloePottier <chloe.pottier@free.fr>
2021-08-12 16:06:55 +0200

    galerie jQuery

---
+++ assets/js/gallery.js
@@ -0,0 +1,33 @@
+jQuery(function($){
+    var webGalleryNav= $('#web-gallery');
+    var printGalleryNav= $('#print-gallery');
+    var allGalleryNav= $('#all-gallery');
+    var contentPortfolio = $('#content-portfolio');
+    var contentPrint = $('#content-print');
+    var contentWeb = $('#content-web');
+        $(webGalleryNav).on('click', function(event){
+            contentWeb.addClass('display-flex2');
+            contentWeb.removeClass('display-none');
+            contentPortfolio.removeClass('display-flex2');
+            contentPortfolio.addClass('display-none');
+            contentPrint.removeClass('display-flex2');
+            contentPrint.addClass('display-none');
+        });
+
+        $(printGalleryNav).on('click', function(event){
+            contentPrint.addClass('display-flex2');
+            contentPrint.removeClass('display-none');
+            contentPortfolio.removeClass('display-flex2');
+            contentPortfolio.addClass('display-none');
+            contentWeb.removeClass('display-flex2');
+            contentWeb.addClass('display-none');
+        });
+        $(allGalleryNav).on('click', function(event){
+            contentPortfolio.addClass('display-flex2');
+            contentPortfolio.removeClass('display-none');
+            contentPrint.removeClass('display-flex2');
+            contentPrint.addClass('display-none');
+            contentWeb.removeClass('display-flex2');
+            contentWeb.addClass('display-none');
+        });
+})
