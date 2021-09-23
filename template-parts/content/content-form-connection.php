811bea2135d550e665a8419dac20dbf0c2e75d2a
ChloePottier <chloe.pottier@free.fr>
2021-09-23 15:40:53 +0200

    single page : user connected or not

---
+++ template-parts/content/content-form-connection.php
@@ -0,0 +1,15 @@
+<p class='pb-5'>Pour visualiser cette page, merci de vous connecter.</p>
+<?php wp_login_form(array(
+    'redirect'       => get_permalink(), // par défaut renvoie vers la page courante
+    'label_username' => 'Identifiant',
+    'label_password' => 'Mot de passe',
+    'label_remember' => 'Se souvenir de moi',
+    'label_log_in'   => 'Se connecter',
+    'form_id'        => 'login-form',
+    'id_username'    => 'user-login',
+    'id_password'    => 'user-pass',
+    'id_remember'    => 'rememberme',
+    'id_submit'      => 'wp-submit',
+    'remember'       => true, //afficher l'option se souvenir de moi
+    'value_remember' => false //se souvenir par défaut ?
+));
